f75eda1b2d14ae45d878aa7552b0d826f0ced443
Dennis C <castrodennis97@gmail.com>
2019-03-07 16:34:01 -0500

    Initial Commit

---
+++ jrotc_guidon_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jrotc_guidon_ios
+//
+//  Created by Dennis C on 3/7/19.
+//  Copyright © 2019 Mithar Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
