cc8e208cf71f7c2d342fc19433d346bf1e3e7bb9
Dennis C <castrodennis97@gmail.com>
2019-04-30 00:48:12 -0400

    oh lord here are some changes
    Lets get this bread

---
+++ jrotc_guidon_ios/mcjrotcTableView.swift
@@ -0,0 +1,40 @@
+//
+//  mcjrotcTableView.swift
+//  jrotc_guidon_ios
+//
+//  Created by Dennis C on 4/29/19.
+//  Copyright © 2019 Mithar Studios. All rights reserved.
+//
+
+import UIKit
+var text = ["Chain of Command","Marine Corps History", "Marine Corps Rank Structure", "Cadet Rank Structure", "General Orders", "Leadership  Traits", "Leadership Principles", "Uniform Inspection Check"]
+
+var insidestuff = ["Chain of command goes in here", "Marine Corps History Stuff goes in here", "Marine Corps Rank Structure might go in here", "MCJROTC Cadet rank structure might go in here", "General Orders Definitely goes in here", "Leadership traits goes here", "Leadership Principles goes in here", "Uniform Inspection check goes in here" ]
+
+var myIndex = 0
+
+class mcjrotcTableView: UITableViewController {
+
+
+override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return text.count
+    }
+
+    
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+    cell.textLabel?.text = text[indexPath.row]
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        myIndex = indexPath.row
+        performSegue(withIdentifier: "segue", sender: self)
+    }
+}
+
+
+
