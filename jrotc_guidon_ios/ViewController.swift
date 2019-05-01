9f73cc1bf95acddb4a98e896cae36a28fa739263
Dennis C <castrodennis97@gmail.com>
2019-04-30 22:03:10 -0400

    Got things to work
    Awesome almost there

--- jrotc_guidon_ios/ViewController.swift
+++ jrotc_guidon_ios/ViewController.swift
@@ -9,36 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController{
-    
-
-    
-    // Making all of the labels and string  here?
-    let ajrotc_label = "ajrotc"
-    let mcjrotc_label = "mcjrotc"
-    let afjrotc_label = "afjrotc"
-    let njroc_label = "njrotc"
-    
-    //IBOutlet allows the code to connect to the UI
-    @IBOutlet weak var nameLabel:UILabel!
-    @IBOutlet weak var ajrotcLabel:UILabel!
-    @IBOutlet weak var mcjrotc_menu: UIButton!
-    
-    
-    
-
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        //This disposes of any resources that can be recreated.
-        //Basically this is grabage collection
-        
     }
-
-
 }
 
