db0461dc1376771ddf87e2fdc97fd7fd700ec467
Dennis Castro - Tims Mac <castrodennis97@gmail.com>
2019-06-07 14:36:47 -0400

    Working on views for each phone
    This requires that I make sure to look at if the Height and Weight are either compact or regular and making sure the phones without those views have to be adjusted for. Also make sure we set pin constraints so that views with the same views adjust for their own, or things get really fucky.

--- jrotc_guidon_ios/ViewController.swift
+++ jrotc_guidon_ios/ViewController.swift
@@ -15,5 +15,18 @@ class ViewController: UIViewController{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    /*
+    func collectionVIew(collectionView: UICollectionView,layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)-> CGSize{
+        return CGSize(width:view.frame.width, height: view.frame.height)
+    }
+    
+    override func willTransition(to newCollection: UITraitCollection, with coordinator: UIViewControllerTransitionCoordinator) {
+        print(UIDevice.current.orientation.isLandscape)
+        collectionView.collectionViewLayout.invalidateLayout()
+        
+        let indexPath = IndexPath(item: pageControl.currentPage, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: centeredHorizontally, animated:true)
+        
+    }*/
 }
 
